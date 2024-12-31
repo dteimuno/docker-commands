6aa5540e7883d92f34f9910f780ac9372dec75eb
Dennis Tei-Muno <munodennis@gmail.com>
2024-12-31 04:32:08 -0600

    Create docker-cmds.sh

---
+++ docker-cmds.sh
@@ -0,0 +1,22 @@
+docker
+docker container run --publish 80:80 nginx
+docker container run --publish 80:80 --detach nginx
+docker container stop cf51a6aaa488
+docker container ls -a
+docker container run --publish 80:80 --detach --name webhost nginx
+docker container ls -a
+docker container top webhost
+docker container -- help
+docker container ls -a
+docker container rm -f 29f586463313
+29f586463313
+docker run --name mongo -d mongo
+docker ps
+docker top mongo
+ps
+ps aux
+ps aux | grep
+ps aux | grep mongo
+docker start mongo
+docker ps
+ps aux | grep mongo
